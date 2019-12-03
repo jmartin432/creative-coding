6de86a67fb45a715911bbdc0f4c14b3536111bd0
Justin Martin <jlmartin432@gmail.com>
2019-12-03 13:20:34 -0800

    Adding final project

---
+++ src/project7/public/listen.js
@@ -0,0 +1,58 @@
+const s = (sketch) => {
+
+    let socket;
+    let host = '127.0.0.1';
+    let port = '3001';
+    let width =640;
+    let height = 480;
+    let mainCanvas;
+    let dots = [];
+
+    sketch.setup = () => {
+        let url = `http://${host}:${port}`;
+        socket = io.connect(url);
+        socket.on('fromPd', newDot);
+        mainCanvas = sketch.createCanvas(width, height);
+        mainCanvas.parent('canvas');
+        sketch.colorMode(sketch.HSB)
+    }
+
+    function newDot(data){
+        // console.log('new message', data);
+        let dot;
+        if (data[1] > 0){
+            dot = {
+            pitch: data[1],
+            amp: data[2],
+            // x: 320,
+            // x: Math.random() * 640,
+            // x: data[1],
+            // y: ((data[1] - 200) / 1700) * 480,
+            // y: ((data[2] - 69) / 10) * height,
+            // alpha: 100
+        };
+        // console.log(dot);
+        if (dots.length < 10) {
+            dots.push(dot)
+        } else {
+            dots.shift();
+            dots.push(dot);
+        }
+        }
+
+    }
+
+    function drawDot(item, i) {
+        sketch.fill(180, 100, 100, i/100);
+        sketch.noStroke();
+        sketch.ellipse(320, 480 - ((item.pitch - 200) / 1700) * 480, item.amp, item.amp);
+    }
+
+
+    sketch.draw = () => {
+        sketch.background(51);
+        dots.forEach((item, index) => {drawDot(item, index)})
+    }
+};
+
+let p5_2 = new p5(s);
---
+++ src/project7/public/play.js
@@ -0,0 +1,74 @@
+const s = (sketch) => {
+
+    let socket;
+    let host = '127.0.0.1';
+    let port = '3001';
+    let width = 400;
+    let height = 400;
+    let mainCanvas;
+    let recordButton;
+    let stopButton;
+    let playButton;
+    let connectPdButton;
+    let gainSlider;
+    let metroSlider;
+
+    sketch.setup = () => {
+        let url = `http://${host}:${port}`;
+        socket = io.connect(url);
+        socket.on('fromPd', )
+        mainCanvas = sketch.createCanvas(width, height);
+        mainCanvas.parent('canvas');
+        gainSlider = document.getElementById("gainSlider");
+        gainSlider.addEventListener('input', function(){
+            console.log('gain');
+            socket.emit('pd', ['/gain', parseFloat(gainSlider.value)]);
+        });
+        metroSlider = document.getElementById("metroSlider");
+        metroSlider.addEventListener('input', function(){
+            console.log('metro');
+            socket.emit('pd', ['/metro', parseFloat(metroSlider.value)]);
+        });
+        recordButton = document.getElementById("recordButton");
+        recordButton.addEventListener('click', function(){
+            console.log('record');
+            socket.emit('pd', ['/record']);
+        });
+        playButton = document.getElementById("playButton");
+        playButton.addEventListener('click', function(){
+            console.log('play');
+            socket.emit('pd', ['/play']);
+        });
+        stopButton = document.getElementById("stopButton");
+        stopButton.addEventListener('click', function(){
+            console.log('stop');
+            socket.emit('pd', ['/stop']);
+        });
+        connectPdButton = document.getElementById("connectPd");
+        connectPdButton.addEventListener('click', function(){
+            console.log('connectPd');
+            socket.emit('pd', ['/connectPd', parseInt(this.value)]);
+            this.value = (this.value === '0') ? '1' : '0';
+            this.innerHTML = (this.value === '0') ? 'Disconnect PD' : 'Connect PD';
+        });
+        sketch.createCanvas(400, 400);
+    }
+
+
+    sketch.draw = () => {
+        // sketch.background(51);
+        // sketch.fill('red');
+        // sketch.ellipse(sketch.mouseX, sketch.mouseY, 45, 45);
+    }
+};
+
+let p5_2 = new p5(s);
+
+
+// oscWebSocket = new osc.WebSocketPort({
+        //     url: "ws://127.0.0.1:3003",
+        //     metadata: true
+        // });
+        // oscWebSocket.on("ready", onSocketOpen);
+        // oscWebSocket.on("message", onSocketMessage);
+        // };
