09753d4ed399dd4f9b53a7d52243af3c23334f2e
Justin Martin <jlmartin432@gmail.com>
2019-12-05 14:14:58 -0800

    added perlin noise page

--- src/project7/public/perlin.js
+++ src/project7/public/perlin.js
@@ -3,18 +3,18 @@ const s = (sketch) => {
     let socket;
     let host = '127.0.0.1';
     let port = '3001';
-    let width = 640;
-    let height = 480;
+    let width = 1440;
+    let height = 900;
     let mainCanvas;
     let systems = [];
-    let xOffsetInc = 0.01;
+    let xOffsetInc = .01;
     let totalSystems = 5;
-    let magnitudeFactor = 10;
+    let magnitudeFactor = 15;
     let increment = .1;
     let maxSteps = 100;
-    let accelerationMagnitude = -.05;
-    let lod = 2;
-    let falloff = .9;
+    let accelerationMagnitude = -.07;
+    let lod = 100;
+    let falloff = .1;
     let fullScreenButton;
     let testMessages = [
         ['/bark', 0.32900553941726685, 0.07359276711940765, 0.18524616956710815, 0.5177004337310791, 0.21738919615745544, 0.2545403838157654, 0.05231967568397522, 0, 0,0,0,0,0,0,0,0.006447233259677887, 0.018036864697933197, 0.03594883158802986, 0.5351402759552002, 0.24375838041305542, 0, 0, 0, 0.09293131530284882, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -57,10 +57,9 @@ const s = (sketch) => {
         constructor(x, y, angle, magnitude, color) {
             this.pos = sketch.createVector(x, y);
             this.magnitude = magnitude * magnitudeFactor;
-            this.xOffset = angle;
-            this.vel = p5.Vector.fromAngle(sketch.noise(this.xOffset) * sketch.TWO_PI, magnitude);
+            this.xOffset = (Math.random() * 10000);
+            this.vel = p5.Vector.fromAngle(sketch.noise(this.xOffset) * 4 * sketch.PI, magnitude);
             this.color = color;
-            // this.xOffset = Math.floor(Math.random() * 1000);
             this.step = 0;
             this.size = 10;
         }
@@ -70,7 +69,7 @@ const s = (sketch) => {
             this.xOffset += xOffsetInc;
             if (this.step < maxSteps) {
                 this.size -= .1;
-                this.vel = p5.Vector.fromAngle(sketch.noise(this.xOffset) * sketch.TWO_PI, this.magnitude);
+                this.vel = p5.Vector.fromAngle(sketch.noise(this.xOffset) * 4 * sketch.PI, this.magnitude);
                 this.pos.add(this.vel);
             }
         }
@@ -87,14 +86,12 @@ const s = (sketch) => {
             let systemY = Math.floor(Math.random() * height);
             let systemColor = Math.floor(Math.random() * 360);
             let particles = [];
-            let rotation = Math.floor(Math.random() * 360);
-            let angleIncrement = 360 / data.length - 1;
+            let rotation = Math.floor(Math.random() * 1);
+            let angleIncrement = 12.5 / data.length - 1;
             for (let i = 1; i < data.length; i++){
                 if (data[i] > 0) {
-                    let particle = new Particle(systemX, systemY, rotation + angleIncrement * i, data[i], systemColor);
+                    let particle = new Particle(systemX, systemY, i, data[i], systemColor);
                     particles.push(particle);
-                } else {
-                    particles.push(null);
                 }
             }
             if (systems.length < 5){
@@ -119,7 +116,10 @@ const s = (sketch) => {
     }
 
     sketch.draw = () => {
-        // sketch.background(0);
+        if (sketch.keyIsDown(32)){
+            systems = [];
+            sketch.background(0);
+        }
         for (let i = 0; i < systems.length; i++) {
             drawSystem(systems[i])
         }
